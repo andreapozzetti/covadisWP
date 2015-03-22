4e553e8c485784613443a61c7f149ae20ce78fa9
andrepozz <andrepozz@inwind.it>
2015-03-22 15:08:10 +0100

    test

--- www/js/menu.js
+++ www/js/menu.js
@@ -13,5 +13,17 @@ function onDeviceReady(){
 	  window.history.back()
 	});
 	
+	$( "a.bus-timetable" ).click(function() {
+	  var ref-bus = window.open('http://www.asfautolinee.it/Portals/5/Documents/Orari/I1213/urbano/Urbano-invernale.pdf', '_blank', 'location=no');
+	});
+	
+	$( "a.boat-timetable" ).click(function() {
+	  var ref-boat = window.open('http://www.navigazionelaghi.it/ita/c_orari.asp', '_blank', 'location=no');
+	});
+	
+	$( "a.shopincomo" ).click(function() {
+	  var ref-shopincomo = window.open('shopincomo://', '_system');
+	});
+	
 
 }
