84e8e6ef42b37d87c65c0188477f4bfb8fc8964a
andrepozz <andrepozz@inwind.it>
2015-03-23 17:36:03 +0100

    test

---
+++ www/js/language.js
@@ -0,0 +1,48 @@
+$(function() {
+//document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+var langs = ['en', 'it'];
+var langCode = '';
+var langJS = null;
+
+
+var translate = function (jsdata)
+{	
+	$("[tkey]").each (function (index)
+	{
+		var strTr = jsdata [$(this).attr ('tkey')];
+	    $(this).html (strTr);
+	});
+}
+
+langCode = navigator.globalization.getPreferredLanguage(
+    function (lng) {
+      var lang = lng.value;
+	  alert(lang);
+      return lang.substring(0, 2);
+    },
+    function () {
+		var lang = "it";
+		return lang;
+    }
+  );
+
+
+langCode = navigator.language.substr (0, 2);
+
+console.log(langCode);
+
+if (langs.indexOf(langCode) > (-1)){
+	$.getJSON('js/lang/'+langCode+'.json', translate);
+}
+else{
+	$.getJSON('js/lang/en.json', translate);
+}
+
+} //end of deviceReady
+
+//});
+
+
+
